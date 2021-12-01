2a5d85df68a0ff3d7d00305f526c3a3b2f420445
MathieuPany <matypany@outlook.fr>
2021-12-01 08:44:57 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,63 @@
-import java.util.Scanner;
-
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args )
+			 {
 
 		/* Affichage temporaire */
-		double[][] tabMatrice;
-		tabMatrice = Matrice.UserCreationMatrice();
+		
+		
+	double[][] tabMatrice1;
+	tabMatrice1 = Matrice.UserCreationMatrice();
+	Matrice tabMatrice=new Matrice(tabMatrice1);
+	System.out.println("la matrice  entrée est :\n"+ tabMatrice);
 
-		Matrice matrice = new Matrice(tabMatrice);
-		System.out.println(matrice);
 
-		matrice = matrice.multiply(2);
+	Matrice matrice = new Matrice(tabMatrice1);
+	matrice = matrice.multiply(3);
+	System.out.println( "la multiplication scalaire est:\n" + matrice  );
+	
+	
+	double determinant ;
+	determinant=tabMatrice1.length;
+		System.out.println("le determinant est :" + determinant);
 
-		System.out.println(matrice);
+	matrice = matrice.multiply(matrice);
+	System.out.println(  "la multiplication par une autre matrice  est:\n"+matrice  );
+
+
+	
+		Matrice additionner;
+	additionner=matrice.Additionner(tabMatrice1);
+	System.out.print("l'addition est : \n" + additionner);
+
+		Matrice transpose;
+	transpose=matrice.getTransposee();
+	System.out.print("la transposêe est : \n" + transpose);
+	
+
+	boolean isEstCarree;
+	isEstCarree=matrice.isEstCarree();
+	System.out.println( "Est ce que la matrice est carrée?: " + isEstCarree);
+	
+
+	
+	boolean Reguliere;
+	Reguliere=matrice.isEstReguliere();
+	System.out.println( "Est ce que la matrice est reguliere?: " + Reguliere);
+
+	Matrice inverse;
+	inverse=matrice.getMatriceInverse();
+	System.out.print("la matrice inverse est : \n" + inverse);
+	
+	
+	
+
+
+			}
+
+	
+ 
+
+	
 	}
-}
+    
